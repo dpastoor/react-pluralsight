9fdc86f31ece9e9da7373ed01dc0683274060c97
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-06 14:16:16 -0800

    set up material ui theme'ing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './css/styles.css'
+import mui from 'material-ui'
 import MessageList from './components/MessageList.jsx'
+
+let ThemeManager = new mui.Styles.ThemeManager();
+let Colors = mui.Styles.Colors;
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    ThemeManager.setPalette({
+         primary1Color: Colors.blue500,
+         primary2Color: Colors.blue700,
+      primary3color: Colors.blue100,
+      accent1Color: Colors.pink400
+    });
   }
+
+// want to add colors to context to make available to other components
+  static childContextTypes = {
+    //just declares we will stick on child context, not actually setting it
+    muiTheme: React.PropTypes.object
+  }
+
+  getChildContext() {
+    return {
+      muiTheme: ThemeManager.getCurrentTheme()
+    };
+  }
+
+
   render() {
-    return (
+    return ( 
        <MessageList> </MessageList>
     );
   }
