524324ea87760b69755f42b1aaf87ff10373c0d2
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-06 14:43:36 -0800

    update material ui theme to new api

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './css/styles.css'
-import mui from 'material-ui'
+import mui, {AppBar, Styles} from 'material-ui'
 import MessageList from './components/MessageList.jsx'
 
-let ThemeManager = new mui.Styles.ThemeManager();
-let Colors = mui.Styles.Colors;
-
+const ThemeManager = Styles.ThemeManager
+const MyRawTheme = require('./css/MyRawTheme');
+console.log(MyRawTheme)
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    ThemeManager.setPalette({
-         primary1Color: Colors.blue500,
-         primary2Color: Colors.blue700,
-      primary3color: Colors.blue100,
-      accent1Color: Colors.pink400
-    });
   }
 
 // want to add colors to context to make available to other components
@@ -26,14 +20,17 @@ export default class App extends React.Component {
 
   getChildContext() {
     return {
-      muiTheme: ThemeManager.getCurrentTheme()
+      muiTheme: ThemeManager.getMuiTheme(MyRawTheme)
     };
   }
 
 
   render() {
-    return ( 
-       <MessageList> </MessageList>
+    return (
+        <div>
+          <AppBar title="Awesome Chat App"></AppBar>
+          <MessageList> </MessageList>
+        </div>
     );
   }
 }
